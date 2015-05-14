1be8ad7f23cc5cd59d9f79c7022c481d300a716e
yuping <yuping@wangleideMBP.lan>
2015-05-14 17:00:13 +0800

    Base

---
+++ BaseAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseAnimation
+//
+//  Created by yuping on 15/5/14.
+//  Copyright (c) 2015年 yuping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaseAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaseAnimation
+//
+//  Created by yuping on 15/5/14.
+//  Copyright (c) 2015年 yuping. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
